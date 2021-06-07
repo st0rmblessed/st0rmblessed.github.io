d44a5c802d7000d5db88b95eb3defd28bb596118
nsantosa <nelsonalves93@hotmail.com>
2021-06-07 21:46:12 +0100

    final changes to new portfolio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("button").on("click", function () {
+    $(".menu").on("click", function () {
         $(".dropdown-menu").animate({
             height: 'toggle'
         });
@@ -10,4 +10,57 @@ $(document).ready(function () {
         });
     });
 });
+
+filterSelection("all")
+
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("projects-item");
+  if (c == "all"){
+    c = "";
+  }
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1){
+        addClass(x[i], "show");
+    }
+  }
+}
+
+// Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("project-filter");
+var btns = btnContainer.getElementsByClassName("button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
